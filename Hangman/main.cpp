73d4ac195f2bcdb8313d699c5f9fdeceb0a87f67
bobkingstone <robert.kingstone@gmail.com>
2015-06-22 13:29:48 +0100

    Added guess functionality

--- Hangman/main.cpp
+++ Hangman/main.cpp
@@ -7,27 +7,61 @@
 //
 
 #include <iostream>
+#include <algorithm>
 #include <string>
 #include <locale>
 
 using namespace std;
 
+void displayGuessMessage(int *guessCount)
+{
+    switch (*guessCount) {
+        case 1:
+            cout << "4 guesses left\n";
+            break;
+        case 2:
+            cout << "3 guesses left\n";
+            break;
+        case 3:
+            cout << "2 guesses left\n";
+            break;
+        case 4:
+            cout << "1 guess left\n";
+            break;
+        default:
+            cout << "end of game, you lose\n";
+            break;
+    }
+}
+
+
 int main(int argc, const char * argv[]) {
 
     string word;
+    string blanks;
+    char placeholder;
     char guess;
     int noOfGuesses;
+    bool won;
+    
+    placeholder = '*';
+    won = false;
+    noOfGuesses = 0;
     
     //GET WORD TO GUESS
     cout << "Enter word to guess:";
     cin >> word;
     
-    for( int i =0; i <= word.length(); i++)
+    for( int i =0; i <= word.length()-1; i++)
     {
         word[i] = tolower(word[i]);
-    }
         
-    while (noOfGuesses != 5) {
+        blanks = blanks + placeholder;
+    }
+    
+    cout << "The word to guess is:\n" << blanks << endl;
+    
+    while ( !won && noOfGuesses != 5 ) {
         
         //ASK USER TO GUESS LETTER
         cout << "Guess a letter:\n";
@@ -36,33 +70,37 @@ int main(int argc, const char * argv[]) {
         //CHECK IF LETTER IS IN WORD
         size_t found = word.find(guess);
         
-        while (found !=string::npos) {
-            cout << guess << " is in the word, guess again" << endl;
-            cin >> guess;
-            found = word.find(guess);
+        if (found !=string::npos)
+        {
+            blanks[found] = guess;
+            
+            cout << guess << " is in the word\n" << blanks << "\n" << endl;
+            
+            size_t f = blanks.find(placeholder);
+            
+            if ( f == string::npos )
+            {
+                won = true;
+            }
+            
         }
-        
-        cout << "letter not found\n";
-        
-        noOfGuesses = noOfGuesses + 1;
-        
-        switch (noOfGuesses) {
-            case 1:
-                cout << "4 guesses left\n";
-                break;
-            case 2:
-                cout << "3 guesses left\n";
-                break;
-            case 3:
-                cout << "2 guesses left\n";
-                break;
-            case 4:
-                cout << "1 guess left\n";
-                break;
-            default:
-                cout << "end of game, you lose\n";
-                break;
+        else
+        {
+            cout << "letter not found\n";
+            
+            noOfGuesses = noOfGuesses + 1;
+            
+            displayGuessMessage(&noOfGuesses);
         }
+    }
+
+    if (won)
+    {
+        cout << "congratulations you won";
+    }
+    else
+    {
+        cout << "you lost the word was\n" << word << endl;
     }
     
     return 0;
